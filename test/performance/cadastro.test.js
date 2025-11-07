de2f439e31e002b9289417266d348c487985c8f2
Matheus Viana <matheusviana762z@gmail.com>
2025-11-07 17:44:04 -0300

    finalizou teste de perfomance

--- test/performance/cadastro.test.js
+++ test/performance/cadastro.test.js
@@ -1,34 +1,35 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
+const postCadastro = JSON.parse(open('../fixtures/postCadastro.json'));
 
 export const options = {
-    iterations: 10,
+    stages: [
+        { duration: '5s', target: 10 },
+        { duration: '20s', target: 10 },
+        { duration: '5s', target: 0 },
+    ],
     thresholds: {
-        http_req_duration: ['p(95)<500', 'max<1000'],
+        http_req_duration: ['p(90)<3000', 'max<5000'],
         http_req_failed: ['rate<0.01'],
     },
 };
 
 export default function () {
-    const url = 'http://localhost:3000/api/gerente/registrar';
-    const payload = JSON.stringify({
-        nome: 'Matheus',
-        email: 'matheusperformance@email.com',
-        senha: '123456',
-    });
+    const BASE_URL = __ENV.BASE_URL || 'http://localhost:3000';
+    const url = `${BASE_URL}/api/gerente/registrar`;
+
+    const payload = JSON.stringify(postCadastro);
 
     const params = {
-        headers: {
-            'Content-Type': 'application/json',
-        },
+        headers: { 'Content-Type': 'application/json' },
     };
 
     const resposta = http.post(url, payload, params);
 
     check(resposta, {
         'Status é 201': (r) => r.status === 201,
-        'resposta tem id': (r) => r.json().hasOwnProperty('id'),
+        'Resposta tem id': (r) => r.json() && r.json().hasOwnProperty('id'),
     });
 
     sleep(1);
